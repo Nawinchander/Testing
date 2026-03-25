bb661cc33ef63cbf33e6691fb45c8fb752778350
Nawin chander <nawinnapzy289@gmail.com>
2026-03-25 10:51:34 +0530

    add the ocde

---
+++ Test/API Testing.js
@@ -0,0 +1,13 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app');
+
+test('GET /health returns status ok', async () => {
+  const res = await request(app).get('/health');
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.status).toBe('ok');
+});
+
+
+
