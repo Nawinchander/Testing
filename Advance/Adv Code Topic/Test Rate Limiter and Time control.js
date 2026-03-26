908c185fed000ed5a839cc80c19a3cdbda70e355
Nawin chander <nawinnapzy289@gmail.com>
2026-03-26 11:41:17 +0530

    add code

--- Advance/Adv Code Topic/Test Rate Limiter and Time control.js
+++ Advance/Adv Code Topic/Test Rate Limiter and Time control.js
@@ -2,3 +2,21 @@
 // Use Jest fake timers (VERY ADVANCED)
 
 
+// rateLimiter.js
+function rateLimiter(limit, windowMs) {
+  let calls = [];
+
+  return () => {
+    const now = Date.now();
+    calls = calls.filter(t => now - t < windowMs);
+
+    if (calls.length >= limit) return false;
+
+    calls.push(now);
+    return true;
+  };
+}
+
+module.exports = { rateLimiter };
+
+
