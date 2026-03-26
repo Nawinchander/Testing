41d2fbe9abc2a700a052ab899cf318eedb494c38
Nawin chander <nawinnapzy289@gmail.com>
2026-03-26 11:43:35 +0530

    add code

--- Advance/Adv Code Topic/Testing Middleware Chain.js
+++ Advance/Adv Code Topic/Testing Middleware Chain.js
@@ -2,5 +2,18 @@
 // Real-world: Express middleware pipeline
 
 
+// middleware.js
+function auth(req, res, next) {
+  if (!req.user) return res.status(401).send('Unauthorized');
+  next();
+}
+
+function handler(req, res) {
+  res.send('Success');
+}
+
+module.exports = { auth, handler };
+
+
 
 
